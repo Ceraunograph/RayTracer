52a7d9fde27c03389f77e6a64289d545db16c2ed
Ceraunograph <takashi.honda@gmail.com>
2013-10-12 18:14:45 -0700

    help

--- Light.cpp
+++ Light.cpp
@@ -1,4 +1,40 @@
 #include "Light.h"
 
+
+void setValue(Point _source, Vector _dir, bool _directional, bool _point, Color _color){
+	directional = _directional;
+	point = _point;
+	if (directional) {
+		dir = _dir;
+	}else if (point) {
+		source = _source;
+	}
+	color = _color;
+}
 void generateLightRay(LocalGeo& local, Ray* lray, Color* lcolor){
+	Ray lightRay;
+	Color lightColor;
+	if (directional){
+		lightRay.pos.setValue(local.pos.x, local.pos.y, local.pos.z);
+		lightRay.dir.setValue(dir.x, dir.y, dir.z);
+                lightRay.t_min = 0.0;
+		lightRay.t_max = std::numeric_limits<float>::max();
+
+		lightColor.setValue(color.r, color.g, color.b);
+
+		lcolor = &lightColor;
+		lray = &lightRay;
+	}else if (point) {
+		lightRay.pos.setValue(local.pos.x, local.pos.y, local.pos.z);
+		lightRay.dir.createFromPoints(lightRay.pos, source);
+		lightRay.t_min = 0.0;
+		lightRay.t_max = std::numeric_limits<float>::max();
+
+		lightColor.setValue(color.r, color.g, color.b);
+
+		lcolor = &lightColor;
+		lray = &lightRay;
+	}else{
+		// throw an exception maybe?
+	}
 }
--- Light.h
+++ Light.h
@@ -10,8 +10,19 @@ class Ray;
 class Color;
 
 class Light {
+<<<<<<< HEAD
 public:
 	virtual void generateLightRay(LocalGeo& local, Ray* lray, Color* lcolor);
+=======
+	Point source;
+	Vecotr dir;
+	bool directional;
+	bool point;
+	Color color;
+
+	void setValue(Point _source, bool _directional, bool _point);
+	void generateLightRay(LocalGeo& local, Ray* lray, Color* lcolor);
+>>>>>>> 4b5c2bc2cdfdbe2ee6162df0ca3aef6efb926d66
 };
 
 #endif

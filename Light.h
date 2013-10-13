4b5c2bc2cdfdbe2ee6162df0ca3aef6efb926d66
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-10-13 01:59:57 -0700

    Modified Lights for point and directional lights

--- Light.h
+++ Light.h
@@ -10,7 +10,14 @@ class Ray;
 class Color;
 
 class Light {
-	virtual void generateLightRay(LocalGeo& local, Ray* lray, Color* lcolor);
+	Point source;
+	Vecotr dir;
+	bool directional;
+	bool point;
+	Color color;
+
+	void setValue(Point _source, bool _directional, bool _point);
+	void generateLightRay(LocalGeo& local, Ray* lray, Color* lcolor);
 };
 
 #endif

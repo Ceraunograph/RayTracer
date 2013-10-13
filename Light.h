5476fc07ad22c5033147a94fcd55264979266fd3
Ceraunograph <takashi.honda@gmail.com>
2013-10-12 18:21:03 -0700

    what

--- Light.h
+++ Light.h
@@ -10,25 +10,16 @@ class Ray;
 class Color;
 
 class Light {
-<<<<<<< HEAD
-<<<<<<< HEAD
 public:
 	virtual void generateLightRay(LocalGeo& local, Ray* lray, Color* lcolor);
-=======
-=======
->>>>>>> 4b5c2bc2cdfdbe2ee6162df0ca3aef6efb926d66
 	Point source;
-	Vecotr dir;
+	Vector dir;
 	bool directional;
 	bool point;
 	Color color;
 
 	void setValue(Point _source, bool _directional, bool _point);
 	void generateLightRay(LocalGeo& local, Ray* lray, Color* lcolor);
-<<<<<<< HEAD
->>>>>>> 4b5c2bc2cdfdbe2ee6162df0ca3aef6efb926d66
-=======
->>>>>>> 4b5c2bc2cdfdbe2ee6162df0ca3aef6efb926d66
 };
 
 #endif
